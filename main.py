3aa4899c4d02f21baac83dbe0c939c1057d2849b
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-03-07 18:48:53 +0200

    :sparkles: add cross-origin resource sharing to communicates frontend with a backend

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+from uvicorn import run
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
+from utils import get_config
 from routers import task
 
-app = FastAPI()
+app_cfg = get_config().app
+host = app_cfg.host
+port = app_cfg.port
+path = app_cfg.path
+version = app_cfg.version
+
+app = FastAPI(version=version)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=task.router)
+app.mount(path=f"/{path}", app=StaticFiles(directory="./views", html=True), name="tasks")
+
+if __name__ == "__main__":
+    run(app=app, host=host)
