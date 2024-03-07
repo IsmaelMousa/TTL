550ca2b45cb5c85c8bbce298681cf1281493db3d
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-03-07 18:21:56 +0200

    :bug: fix the behavior of get_db when an exception occurs

--- dependencies/util.py
+++ dependencies/util.py
@@ -1,7 +1,4 @@
-from fastapi.exceptions import ValidationException
-
 from infrastructures import get_database_stuff
-from utils import get_config, get_logger
 
 Session = get_database_stuff().session
 
@@ -12,20 +9,9 @@ def get_db() -> Session:
 
     :return: the database session (instance of Session)
     """
-    app_cfg = get_config().app
-    level = app_cfg.log_level
-    logger = get_logger(level=level)
-
     db = Session()
-
     try:
         yield db
-
-    except ValidationException as exc:  # pragma: no cover
-        logger.error(f"Unprocessable Entity: {exc}")
-
-    except Exception as exc:  # pragma: no cover
-        logger.error(f"Unhandled Exception: {exc}")
 
     finally:
         db.close()
